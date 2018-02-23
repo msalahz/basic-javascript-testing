f5fc9f2137d548fe48073f08cfc93c5d3db8030d
Mohammed Zaghloul <m.salahz86@gmail.com>
2018-02-23 19:21:09 +0200

    Implement first test

---
+++ spec/option1.test.js
@@ -0,0 +1,17 @@
+const main = require('../src/main');
+const Contact = main.Contact;
+const AddressBook = main.AddressBook;
+
+// AddressBook tests - first option
+var adrianContact = new Contact('Adrian');
+var addressBook = new AddressBook();
+addressBook.addContact(adrianContact);
+var errorMessage = 'Should return Adian\'s contact';
+// Passed test - should pint nothing to the console
+console.log('--------Passed test--------');
+console.assert(addressBook.getContact(0) === adrianContact, errorMessage);
+// break the test
+annieContact = new Contact('Annie');
+// Failed test - should print the error message to the console
+console.log('--------Failed test--------');
+console.assert(addressBook.getContact(0) === annieContact, errorMessage);
